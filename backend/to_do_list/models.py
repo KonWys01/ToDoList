6a7de7ab53a26cb105e382543045d8f8e2adacca
KonWys01 <konradwysokinski01@gmail.com>
2022-11-27 22:48:13 +0100

    base of Column and Task

--- backend/to_do_list/models.py
+++ backend/to_do_list/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Column(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    status = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(null=True, blank=True)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_finish = models.DateTimeField(null=True)
+    id_column = models.ForeignKey(Column, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
