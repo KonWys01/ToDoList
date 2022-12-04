14c13d04e4806b59e21af7d3bc195289916fa40f
KonWys01 <konradwysokinski01@gmail.com>
2022-12-04 19:42:19 +0100

    Frontend - moveable cards between categories

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import {MatButtonModule} from "@angular/material/button";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CategoryComponent } from './components/category/category.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {DragDropModule} from "@angular/cdk/drag-drop";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    DragDropModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
---
+++ frontend/src/app/components/category/category.component.ts
@@ -0,0 +1,65 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CategoryComponent {
+  testData_to_do = {
+    "data": [
+      {
+        "id": 1,
+        "name": "Test Task",
+        "status": "To Do"
+      },
+      {
+        "id": 2,
+        "name": "Test Task v2",
+        "status": "To Do v2"
+      },
+      {
+        "id": 1,
+        "name": "Test Task v3",
+        "status": "To Do v3"
+      },
+      {
+        "id": 1,
+        "name": "Test Task v4",
+        "status": "To Do v4"
+      },
+      {
+        "id": 1,
+        "name": "Test Task v5",
+        "status": "To Do v5"
+      },
+    ]
+  }
+  testData_done = {
+    "data": [
+      {
+        "id": 4,
+        "name": "Test Task v4",
+        "status": "To Do v4"
+      },
+    ]
+  }
+
+  constructor() {
+  }
+
+  drop2(event: CdkDragDrop<{ id: number, name: string, status: string }[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+}
